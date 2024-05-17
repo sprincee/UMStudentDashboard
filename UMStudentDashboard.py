800078bd96bb1c37cb05c65de1661b224b35c7ca
sprincee <87615605+sprincee@users.noreply.github.com>
2024-05-17 16:12:48 -0400

    Add files via upload
    Implements the following:
    - The main file.
    
    Note: Pasted from VSCode after development & extensive testing to ensure program functions before commit.

---
+++ UMStudentDashboard.py
@@ -0,0 +1,26 @@
+
+#Final-Project aka UMStudentDashboard
+
+'''
+Coding-Outline:
+Summary: 
+- An academic dashboard -- used primarily to run tests based on grades inputted.
+Object:
+- To provide a user with valuable information from which they can make academic decisions.
+Parameters:
+- A user will input student-ID and name only ONCE. It will be saved in memory hereafter, preventing unnecessary inputs. 
+- A user will input a floating-point number, which will repersent a given alpha-character (A-F), for each subject.
+- A user can only input a subject ONCE. (e.g the subject cannot appear twice.)
+- A user will be prompted with types of information they'd like presented -- ONLY after they have finished inputting data/grades. (e.g asking for average-grade distribution)
+- The program will provide two types of data: an average grade graph and a grade-frequency graph.
+Notes:
+- Taking inspiration from StudentVUE -- an app used in K-12 education.
+Functions/Structural Elements:
+- An empty list to store subjects -- checked for duplicates, since those are not allowed.
+- An empty list to store names -- checked for duplicates, since those are not allowed.
+- An empty list to store student-ID's -- checked for duplicates, since those are not allowed
+A note for grading:
+- I used this as an initial commit; also as a way to organize my thoughts.
+- It's not all encompassing & things are probably going to change during dev cycle.
+- It's just a way to get my ideas on paper (or screen, in this case).
+'''
